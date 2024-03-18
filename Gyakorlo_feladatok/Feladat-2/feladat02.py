bc84e42436f8aef3bc506c1727a388a7ba9d54e5
nszadmin <nszadmin@nszadmin.com>
2024-03-18 13:56:33 +0100

    added

--- Gyakorlo_feladatok/Feladat-2/feladat02.py
+++ Gyakorlo_feladatok/Feladat-2/feladat02.py
@@ -1,1 +1,30 @@
-print("")
+# importáljuk a random nevű könyvtárat a használhathoz
+import random;
+
+# Mivel egy végtelen programot szeretnénk ezért csinálunk egy végtelen ciklust
+# Persze a végtelen ciklusokat inkább kerüljük az életbe előfordulnak élethelyzetek 
+# amikor használni kell
+
+print("Melyik számra gondoltam")
+probalkozasok = 10
+random_szam = random.randint(0,100)
+run = True
+
+while run:
+    felhaszanlo_valasz = int(input())
+    if(probalkozasok > 0):
+        if(random_szam == felhaszanlo_valasz):
+            print("A válasz helyes győztél")
+            run = False
+        else:
+            print("A válasz helytelen")
+            probalkozasok -= 1
+            print("Hátralévő probálkozások száma %s" % (probalkozasok+1))
+    else:
+        print("Elfogyott az összes probálkozásod")
+        print("A helyes megoldás a %s volt" % (random_szam))
+        random_szam = random.randint(0,100)
+        probalkozasok += 10
+
+
+    
